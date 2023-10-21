ef8f2cd1ffdfb7755a3a2c30114572fe6e111a6c
Seba <sebastiantorresola3@gmail.com>
2023-10-21 15:18:44 -0700

    Added routing

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'expo-router'
 import { View, Text } from 'react-native'
 
 const index = () => {
   return (
     <View>
       <Text>Hi</Text>
+      <Link href="/modes">Get Start</Link>
     </View>
   )
 }
