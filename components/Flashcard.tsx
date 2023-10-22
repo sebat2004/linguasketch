c7de26bd37ad68f78b1c48baa678a3af35d8025d
Carson Secrest <carson@10-248-110-2.wireless.oregonstate.edu>
2023-10-22 15:44:31 -0700

    Changed the vocab formatting in vocab.json. Created code to search for the vocab word corresponding with the selected category and language and display it along with its English translation.

--- components/Flashcard.tsx
+++ components/Flashcard.tsx
@@ -1,20 +1,43 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import vocab from '../app/vocab.json'
+import { View, Text } from 'react-native';
+import React from 'react';
+import vocab from '../app/vocab.json';
 
 type FlashcardProps = {
-    language: string,
-    category: string
-}
+  language: string;
+  category: string;
+};
 
 const Flashcard = (props: FlashcardProps) => {
+  const { language, category } = props;
+  const vocabData: any = vocab;
+
+  const getRandomWord = (category: string, language: string) => {
+    const categoryData = vocabData[category];
+
+    const englishWords = categoryData['english'];
+    const languageWords = categoryData[language];
+
+    const translations = Object.keys(englishWords);
+    const words = Object.keys(languageWords);
+    const randomIndex = Math.floor(Math.random() * words.length);
+    const translation = translations[randomIndex + 1];
+    const word = words[randomIndex + 1];
+
+    return (
+      <View style={{ backgroundColor: 'white', width: 300 }}>
+        <Text>Category: {category}</Text>
+        <Text>Language: {language}</Text>
+        <Text>English Word: {englishWords[translation]}</Text>
+        <Text>Translation in {language}: {languageWords[word]}</Text>
+      </View>
+    );
+  };
 
   return (
-    <View style={{backgroundColor: 'white', width: 300}}>
-      <Text>{props.language}</Text>
-      <Text>{props.category}</Text>
+    <View style={{ backgroundColor: 'white', width: 300 }}>
+      {getRandomWord(category, language)}
     </View>
-  )
-}
+  );
+};
 
 export default Flashcard;
