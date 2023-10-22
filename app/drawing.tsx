23f31146a5ebbe3562f639746809b7d7b43e0b33
Seba <sebastiantorresola3@gmail.com>
2023-10-22 15:48:02 -0700

    Added safe area viewing

--- app/drawing.tsx
+++ app/drawing.tsx
@@ -2,17 +2,21 @@ import { View, Text, Button } from 'react-native'
 import Flashcard from '../components/Flashcard.tsx'
 import { useState } from 'react'
 import DrawingBoard from '../components/DrawingBoard.tsx'
-import { useLocalSearchParams } from 'expo-router'
+import { useLocalSearchParams, Stack } from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const drawing = () => {
   const [isFlipped, setIsFlipped] = useState(false)
   const { language, category }: any = useLocalSearchParams()
 
   return (
-    <View style={{flex: 1}}>
-      {isFlipped ? <DrawingBoard /> : <Flashcard category={category} language={language} />}
-      <Button title="Flip" onPress={() => setIsFlipped(!isFlipped)} />
-    </View>
+    <>
+      <Stack.Screen options={{headerShown: false}} />
+      <SafeAreaView>
+        {isFlipped ? <DrawingBoard /> : <Flashcard category={category} language={language} />}
+        <Button title="Flip" onPress={() => setIsFlipped(!isFlipped)} />
+      </SafeAreaView>
+    </>
   )
 }
 
