dc1d004dfbce8aacb9cf20acc51f47cff1002ee4
Seba <sebastiantorresola3@gmail.com>
2023-10-22 19:16:14 -0700

    Merged conflict with Flashcard.tsx file

--- components/Flashcard.tsx
+++ components/Flashcard.tsx
@@ -10,26 +10,8 @@ type FlashcardProps = {
 }
 
 const Flashcard = (props: FlashcardProps) => {
-<<<<<<< HEAD
   const { language, category, eWord, word } = props;
   
-=======
-  const { language, category } = props;
-  const vocabData: any = vocab;
-
-  const getRandomWord = (category: string, language: string) => {
-    const categoryData = vocabData[category];
-
-    const englishWords = categoryData['english'];
-    const languageWords = categoryData[language];
-
-    const translations = Object.keys(englishWords);
-    const words = Object.keys(languageWords);
-    const randomIndex = Math.floor(Math.random() * words.length);
-    const translation = translations[randomIndex];
-    const word = words[randomIndex];
-
->>>>>>> 7f1962dc9d6c95541d3db33f1d09801174187507
     return (
       <View style={{ backgroundColor: 'white', width: 300 }}>
         <Text>Category: {category}</Text>
