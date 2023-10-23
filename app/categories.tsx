f38526ff5b40aa0bbb151f0b025af06a206b53fb
Carson Secrest <carson@10-248-110-2.wireless.oregonstate.edu>
2023-10-22 18:17:11 -0700

    Added and styled buttons on languages and categories pages.

--- app/categories.tsx
+++ app/categories.tsx
@@ -1,99 +1,119 @@
-import { View, Text } from 'react-native'
-import { Link, useLocalSearchParams, Stack } from 'expo-router'
-import React, { useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import CustomHeader from '../components/CustomHeader'
+import { View, Text } from 'react-native';
+import { Link, useLocalSearchParams, Stack } from 'expo-router';
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import CustomHeader from '../components/CustomHeader';
 
-const categories = () => {
-  const params: any = useLocalSearchParams() 
-  const [language, setLanguage] = useState<any>(params.language)
+const Categories = () => {
+  const params: any = useLocalSearchParams();
+  const [language, setLanguage] = useState<any>(params.language);
 
   return (
     <>
-      <CustomHeader language={language}/>
+      <CustomHeader language={language} />
       <Stack.Screen options={{headerShown: false}} />
-      <SafeAreaView>
-        <Link 
-          href={{
-            pathname: "/drawing",
-            params: {
-              language: language,
-              category: 'transportation'
-            }
-          }}
-        >
-          Transportation
-        </Link>
-        <Link 
-          href={{
-            pathname: "/drawing",
-            params: {
-              language: language,
-              category: 'colors'
-            }
-          }}
-        >
-          Colors
-        </Link>
-        <Link 
-          href={{
-            pathname: "/drawing",
-            params: {
-              language: language,
-              category: 'food'
-            }
-          }}
-        >
-          Food
-        </Link>
-        <Link 
-          href={{
-            pathname: "/drawing",
-            params: {
-              language: language,
-              category: 'clothing'
-            }
-          }}
-        >
-          Clothing
-        </Link>
-        <Link 
-          href={{
-            pathname: "/drawing",
-            params: {
-              language: language,
-              category: 'occupations'
-            }
-          }}
-        >
-          Occupations
-        </Link>
-        <Link 
-          href={{
-            pathname: "/drawing",
-            params: {
-              language: language,
-              category: 'sports'
-            }
-          }}
-        >
-          Sports
-        </Link>
-        <Link 
-          href={{
-            pathname: "/review",
-            params: {
-              language: language,
-              category: 'sports'
-            }
-          }}
-        >
-          Review
-        </Link>
+      <SafeAreaView style={{flex: 1, backgroundColor: '#F4EBE4'}}>
+        <View style={{flex: 1, flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-around', alignItems: 'center'}}>
+          
+          {/* Column 1 */}
+          <View>
+            <Link 
+              href={{
+                pathname: "/drawing",
+                params: {
+                  language: language,
+                  category: 'transportation'
+                }
+              }}
+              style={styles.link}
+            >
+              Transportation
+            </Link>
+            <Link 
+              href={{
+                pathname: "/drawing",
+                params: {
+                  language: language,
+                  category: 'colors'
+                }
+              }}
+              style={styles.link}
+            >
+              Colors
+            </Link>
+            <Link 
+              href={{
+                pathname: "/drawing",
+                params: {
+                  language: language,
+                  category: 'food'
+                }
+              }}
+              style={styles.link}
+            >
+              Food
+            </Link>
+          </View>
+          
+          {/* Column 2 */}
+          <View>
+            <Link 
+              href={{
+                pathname: "/drawing",
+                params: {
+                  language: language,
+                  category: 'clothing'
+                }
+              }}
+              style={styles.link}
+            >
+              Clothing
+            </Link>
+            <Link 
+              href={{
+                pathname: "/drawing",
+                params: {
+                  language: language,
+                  category: 'occupations'
+                }
+              }}
+              style={styles.link}
+            >
+              Occupations
+            </Link>
+            <Link 
+              href={{
+                pathname: "/drawing",
+                params: {
+                  language: language,
+                  category: 'sports'
+                }
+              }}
+              style={styles.link}
+            >
+              Sports
+            </Link>
+          </View>
+        </View>
       </SafeAreaView>
     </>
   );
 };
 
+const styles = {
+  link: { 
+    fontSize: 30,
+    marginTop: 20,
+    width: 150,
+    height: 50,
+    color: '#E4CDBC',
+    textAlign: "center",
+    lineHeight: 50,
+    backgroundColor: '#695648',
+    borderRadius: 10,
+    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+    cursor: 'pointer',
+  },
+};
 
-export default categories;
+export default Categories;
--- app/languges.tsx
+++ app/languges.tsx
@@ -1,97 +1,90 @@
-import { View, Text } from 'react-native'
-import { Link, Stack } from 'expo-router'
-import React, { useState } from 'react'
-import { Image } from 'react-native'
-import { Screen } from 'expo-router/build/views/Screen'
+import { View, Text } from 'react-native';
+import { Link, Stack } from 'expo-router';
+import React, { useState } from 'react';
+import { Image } from 'react-native';
+import { Screen } from 'expo-router/build/views/Screen';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-const languges = () => {
+const Languages = () => {
   const [language, setLanguage] = useState('spanish');
 
   return (
     <>
       <Stack.Screen options={{headerShown: false}} />
-      <SafeAreaView style={{flex: 1, alignItems: 'center'}}>
-        <Image style={{width: 150, height: 140, marginTop: 60}} source={require('../assets/images/beav.png')} />
-        <View style={{flex: 4, marginHorizontal: "auto", width: "100%"}}>
-          <View style={{flexDirection: "row", marginTop: 180}}>
-            <View style={styles.column}>
-              <Link
-                href={{
-                  pathname: "/categories",
-                  params: { language: 'spanish' }
-                }}
-                style={styles.flag}
-                onPress={() => setLanguage('spanish')}
-              >
-                Spanish
-              </Link>
-              <Link 
-                href={{
-                  pathname: "/categories",
-                  params: { language: 'french' }
-                }}
-                style={styles.flag}
-                onPress={() => setLanguage('french')}
-              >
-                French
-              </Link>
-            </View>
-            <View style={styles.column}>
-              <Link 
-                href={{
-                  pathname: "/categories",
-                  params: { language: 'chinese' }
-                }}
-                style={styles.flag}
-                onPress={() => setLanguage('chinese')}
-              >
-                Chinese
-              </Link>
-              <Link 
-                href={{
-                  pathname: "/categories",
-                  params: { language: 'german' }
-                }}
-                style={styles.flag}
-                onPress={() => setLanguage('german')}
-              >
-                German
-              </Link>
-            </View>
-          </View>
+      <SafeAreaView style={{flex: 1, backgroundColor: '#F4EBE4'}}>
+        <Image style={{width: 330, height: 300, marginTop: 30, marginLeft: 80}} source={require('../assets/images/bubble1.png')} />
+        <View style={{flex: 4, marginHorizontal: "auto", width: "100%", alignItems: 'center', marginTop: -10}}>
+          <Link
+            href={{
+              pathname: "/categories",
+              params: { language: 'spanish' }
+            }}
+            style={styles.flag}
+            onPress={() => setLanguage('spanish')}
+          >
+            Spanish
+          </Link>
           <Link 
-              href={{
-                pathname: "/categories",
-                params: { language: 'korean' }
-              }}
-              style={{ alignSelf: 'center', paddingTop: 50}}
-              onPress={() => setLanguage('korean')}
-            >
-              Korean
-            </Link>
+            href={{
+              pathname: "/categories",
+              params: { language: 'french' }
+            }}
+            style={styles.flag}
+            onPress={() => setLanguage('french')}
+          >
+            French
+          </Link>
+          <Link 
+            href={{
+              pathname: "/categories",
+              params: { language: 'chinese' }
+            }}
+            style={styles.flag}
+            onPress={() => setLanguage('chinese')}
+          >
+            Chinese
+          </Link>
+          <Link 
+            href={{
+              pathname: "/categories",
+              params: { language: 'german' }
+            }}
+            style={styles.flag}
+            onPress={() => setLanguage('german')}
+          >
+            German
+          </Link>
+          <Link 
+            href={{
+              pathname: "/categories",
+              params: { language: 'korean' }
+            }}
+            style={{...styles.flag, marginTop: 20}}
+            onPress={() => setLanguage('korean')}
+          >
+            Korean
+          </Link>
         </View>
       </SafeAreaView>
     </>
   );
 };
 
-// Styles
 const styles = {
   flag: { 
-    flexDirection: "row",
-    width: 75, 
+    fontSize: 30,
+    marginTop: 20,
+    width: 300, 
     height: 50,
-    backgroundColor: 'red',
+    color: '#E4CDBC',
+    textAlign: "center",
+    lineHeight: 50,
+    backgroundColor: '#695648',
     backgroundImage: 'url(../assets/images/flag.png)',
-  },
-  column: {
-    flex: 2, 
-    alignItems: 'center', 
-    width: 100, 
-    gap: 60,
+    borderRadius: 10,
+    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+    cursor: 'pointer',
   },
 };
 
-
-export default languges
+export default Languages;
